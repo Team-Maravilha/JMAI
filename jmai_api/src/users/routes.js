60377ed6e7c6f68af2adaeaaf9999c095b8becca
Rui Cruz <ruicrux@hotmail.com>
2023-12-21 15:16:12 +0000

    API
    - Adicionar Administrador
    - Adicionar Médico

--- jmai_api/src/users/routes.js
+++ jmai_api/src/users/routes.js
@@ -5,6 +5,9 @@ const authMiddleware = require('../../authMiddleware');
 const router = Router();
 
 router.get('/listar/tabela', authMiddleware, controller.ListarUtilizadoresDataTable);
+router.get('/registar/administrador', authMiddleware, controller.RegistarAdministrador);
 router.post('/registar/medico', authMiddleware, controller.RegistarMedico);
+router.post('/registar/rececionista', authMiddleware, controller.RegistarRececionista);
+
 
 module.exports = router;

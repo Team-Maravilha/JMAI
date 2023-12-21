729fcf7a6da58bef551716415822de46de5870ed
Thays Vieira <svieirathays@gmail.com>
2023-12-21 23:18:42 +0000

    Update requerimento.php
    Criação do dropzonejs

--- jmai_app/pages/requerimento.php
+++ jmai_app/pages/requerimento.php
@@ -231,20 +231,82 @@
                                                                 </div>
                                                             </div>
                                                         </div>
+                                                        <div class="separator border-5 my-6"></div>
+
+
+                                                        <!--begin::Input group-->
+                                                        <div class="form-group row col-12">
+                                                            <!--begin::Label-->
+                                                            <label class="col-lg-6 col-form-label text-lg-right">Anexe os documentos que achar necessário para a avaliação do seu requerimento:</label>
+                                                            <!--end::Label-->
+
+                                                            <!--begin::Col-->
+                                                            <div class="col-lg-10">
+                                                                <!--begin::Dropzone-->
+                                                                <div class="dropzone dropzone-queue mb-2" id="kt_dropzonejs_example_3">
+                                                                    <!--begin::Controls-->
+                                                                    <div class="dropzone-panel mb-lg-0 mb-2">
+                                                                        <a class="dropzone-select btn btn-sm btn-primary me-2">Anexar Documentos</a>
+                                                                        <a class="dropzone-upload btn btn-sm btn-light-primary me-2">Carregar Todos</a>
+                                                                        <a class="dropzone-remove-all btn btn-sm btn-light-primary">Remover Todos</a>
+                                                                    </div>
+                                                                    <!--end::Controls-->
+
+                                                                    <!--begin::Items-->
+                                                                    <div class="dropzone-items wm-200px">
+                                                                        <div class="dropzone-item" style="display:none">
+                                                                            <!--begin::File-->
+                                                                            <div class="dropzone-file">
+                                                                                <div class="dropzone-filename" title="some_image_file_name.jpg">
+                                                                                    <span data-dz-name>some_image_file_name.jpg</span>
+                                                                                    <strong>(<span data-dz-size>340kb</span>)</strong>
+                                                                                </div>
+
+                                                                                <div class="dropzone-error" data-dz-errormessage></div>
+                                                                            </div>
+                                                                            <!--end::File-->
+
+                                                                            <!--begin::Progress-->
+                                                                            <div class="dropzone-progress">
+                                                                                <div class="progress">
+                                                                                    <div class="progress-bar bg-primary" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0" data-dz-uploadprogress>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                            <!--end::Progress-->
+
+                                                                            <!--begin::Toolbar-->
+                                                                            <div class="dropzone-toolbar">
+                                                                                <span class="dropzone-delete" data-dz-remove><i class="bi bi-x fs-1"></i></span>
+                                                                            </div>
+                                                                            <!--end::Toolbar-->
+                                                                        </div>
+                                                                    </div>
+                                                                    <!--end::Items-->
+                                                                </div>
+                                                                <!--end::Dropzone-->
+
+                                                                <!--begin::Hint-->
+                                                                <span class="form-text text-muted">O tamanho máximo do ficheiro é de 1MB e o número máximo de ficheiros é de 5.</span>
+                                                                <!--end::Hint-->
+                                                            </div>
+                                                            <!--end::Col-->
+                                                        </div>
+                                                        <!--end::Input group-->
+
+
+                                                        <div class="card-footer d-flex justify-content-end py-6 px-9 g-5">
+                                                            <button type="reset" class="btn btn-light btn-active-light-primary me-2">Cancelar</button>
+                                                            <button type="submit" class="btn btn-primary" data-element="submit">
+                                                                <span class="indicator-label">
+                                                                    Submeter
+                                                                </span>
+                                                                <span class="indicator-progress">
+                                                                    Por favor Aguarde... <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
+                                                                </span>
+                                                            </button>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div class="card-footer d-flex justify-content-end py-6 px-9 g-5">
-                                                    <button type="reset" class="btn btn-light btn-active-light-primary me-2">Cancelar</button>
-                                                    <button type="submit" class="btn btn-primary" data-element="submit">
-                                                        <span class="indicator-label">
-                                                            Submeter
-                                                        </span>
-                                                        <span class="indicator-progress">
-                                                            Por favor Aguarde... <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
-                                                        </span>
-                                                    </button>
-                                                </div>
-                                            </div>
                                         </form>
                                     </div>
                                 </div>
@@ -424,6 +486,69 @@
                 carregarDistritosNaturalidade($(this).val());
             });
 
+        });
+    </script>
+
+    <script>
+        // set the dropzone container id
+        const id = "#kt_dropzonejs_example_3";
+        const dropzone = document.querySelector(id);
+
+        // set the preview element template
+        var previewNode = dropzone.querySelector(".dropzone-item");
+        previewNode.id = "";
+        var previewTemplate = previewNode.parentNode.innerHTML;
+        previewNode.parentNode.removeChild(previewNode);
+
+        var myDropzone = new Dropzone(id, { // Make the whole body a dropzone
+            url: "https://keenthemes.com/scripts/void.php", // Set the url for your upload script location
+            parallelUploads: 20,
+            maxFiles: 5,
+            maxFilesize: 1, // Max filesize in MB
+            previewTemplate: previewTemplate,
+            previewsContainer: id + " .dropzone-items", // Define the container to display the previews
+            clickable: id + " .dropzone-select", // Define the element that should be used as click trigger to select files.
+            //LOCALE
+            dictFileTooBig: "O ficheiro é demasiado grande ({{filesize}}MiB). Tamanho máximo: {{maxFilesize}}MiB.",
+            dictInvalidFileType: "Não pode carregar ficheiros deste tipo.",
+            dictResponseError: "O servidor respondeu com o código {{statusCode}}.",
+
+        });
+
+        myDropzone.on("addedfile", function(file) {
+            // Hookup the start button
+            const dropzoneItems = dropzone.querySelectorAll('.dropzone-item');
+            dropzoneItems.forEach(dropzoneItem => {
+                dropzoneItem.style.display = '';
+            });
+        });
+
+        // Update the total progress bar
+        myDropzone.on("totaluploadprogress", function(progress) {
+            const progressBars = dropzone.querySelectorAll('.progress-bar');
+            progressBars.forEach(progressBar => {
+                progressBar.style.width = progress + "%";
+            });
+        });
+
+        myDropzone.on("sending", function(file) {
+            // Show the total progress bar when upload starts
+            const progressBars = dropzone.querySelectorAll('.progress-bar');
+            progressBars.forEach(progressBar => {
+                progressBar.style.opacity = "1";
+            });
+        });
+
+        // Hide the total progress bar when nothing"s uploading anymore
+        myDropzone.on("complete", function(progress) {
+            const progressBars = dropzone.querySelectorAll('.dz-complete');
+
+            setTimeout(function() {
+                progressBars.forEach(progressBar => {
+                    progressBar.querySelector('.progress-bar').style.opacity = "0";
+                    progressBar.querySelector('.progress').style.opacity = "0";
+                });
+            }, 300);
         });
     </script>
 
